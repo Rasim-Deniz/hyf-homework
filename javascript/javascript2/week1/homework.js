3b0d42e71a2a8cd4d1af2abb40df145ae91732cd
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-02-26 01:49:26 +0100

    Implemented mentors feedback

--- javascript/javascript2/week1/homework.js
+++ javascript/javascript2/week1/homework.js
@@ -2,61 +2,71 @@
 
 const danishWords = ["bil", "plante", "kaffe", "bog", "ø", "planetarium"];
 
-function findShortest(words) {
-    let shortestWord = words[0];
-    words.forEach(element => {
-        if (shortestWord.length > element.length) {
-            shortestWord = element;
-        }
-    });
-    return shortestWord;
+function findShortestWord(words) {
+  let shortestWord = words[0];
+  words.forEach((element) => {
+    if (shortestWord.length > element.length) {
+      shortestWord = element;
+    }
+  });
+  return shortestWord;
 }
-console.log(findShortest(danishWords));
+console.log(findShortestWord(danishWords));
 
 // Find and count the Danish letters
 
 function findDanishLetter(sentence) {
-    let æ = (sentence.match(/æ/gi) || []).length;
-    let ø = (sentence.match(/ø/gi) || []).length;
-    let å = (sentence.match(/å/gi) || []).length;
-    let total = æ + ø + å;
-    let object = {
-        total,
-        æ,
-        ø,
-        å
+  let æ = (sentence.match(/æ/gi) || []).length;
+  let ø = (sentence.match(/ø/gi) || []).length;
+  let å = (sentence.match(/å/gi) || []).length;
+  let total = æ + ø + å;
+  let object = {
+    total,
+    æ,
+    ø,
+    å,
+  };
+  for (const property in object) {
+    if (object[property] === 0) {
+      delete object[property];
     }
-    for (const property in object) {
-        if (object[property] === 0) {
-            delete object[property];
-        }
-    }
-    console.log(object);
-};
+  }
+  console.log(object);
+}
 const danishString = "Jeg har en blå bil";
-findDanishLetter(danishString);  // returns {total: 1, å: 1}
+findDanishLetter(danishString); // returns {total: 1, å: 1}
 
 const danishString2 = "Blå grød med røde bær";
 findDanishLetter(danishString2); // returns {total: 4, æ: 1, ø: 2, å: 1}
 
 // Spirit animal name generator
 
-const spiritAnimals = ["Adventurous Wolf", "Mystical Unicorn", "Passionate Leopard", "Intuitive Dolphin", "Amazing Dove", "Victorious Elephant", "Magical Butterfly", "Wild Panther", "Creative Monkey", "Speedy Eagle"];
+const spiritAnimals = [
+  "Adventurous Wolf",
+  "Mystical Unicorn",
+  "Passionate Leopard",
+  "Intuitive Dolphin",
+  "Amazing Dove",
+  "Victorious Elephant",
+  "Magical Butterfly",
+  "Wild Panther",
+  "Creative Monkey",
+  "Speedy Eagle",
+];
 let input = document.getElementById("user-name");
 let button = document.getElementById("submit-button");
 let output = document.getElementById("output");
 button.addEventListener("click", buttonFunction);
 
 function buttonFunction() {
-    if (!input.value) {
-        alert("Please write your name");
-        return;
-    }
-    else {
-        let userName = input.value;
-        let randomAnimal = spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
-        let fullText = `${userName} - ${randomAnimal}`;
-        output.innerHTML = fullText;
-    }
-};
-
+  if (!input.value) {
+    alert("Please write your name");
+    return;
+  } else {
+    let userName = input.value;
+    let randomAnimal =
+      spiritAnimals[Math.floor(Math.random() * spiritAnimals.length)];
+    let fullText = `${userName} - ${randomAnimal}`;
+    output.innerHTML = fullText;
+  }
+}
