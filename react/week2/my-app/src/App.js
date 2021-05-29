53c4c284970ed7bda396deacebc7a074df7adab6
Rasim-Deniz <rasimdeniz1996@gmail.com>
2021-05-29 22:18:33 +0200

    Homework done

--- react/week2/my-app/src/App.js
+++ react/week2/my-app/src/App.js
@@ -1,17 +1,75 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import data from "./components/data.json";
-import Header from "./components/header";
 import TodoList from "./components/todoList";
 
+function Header() {
+  return (
+    <header>
+      <h1>ToDo List</h1>
+    </header>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const counter = setTimeout(() => {
+      setCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearTimeout(counter);
+    };
+  }, [count]);
+
+  return (
+    <p>
+      You have used <span id="count">{count}</span> seconds on this website
+    </p>
+  );
+}
+
+function AddTodo({ addTodo }) {
+  return <button onClick={addTodo}>Add Todo</button>;
+}
+
 function App() {
-  const [todoList] = useState(data);
+  const [todoList, setTodoList] = useState(data);
+
+  const checkDone = (id) => {
+    let mappedList = todoList.map((todo) => {
+      return todo.id === parseInt(id)
+        ? { ...todo, done: !todo.done }
+        : { ...todo };
+    });
+    setTodoList(mappedList);
+  };
+
+  const addTodo = () => {
+    setTodoList([
+      ...todoList,
+      { id: todoList.length + 1, title: "random text", done: false },
+    ]);
+  };
+
+  const removeTodo = (id) => {
+    let filteredList = todoList.filter((todo) => todo.id !== id);
+    setTodoList(filteredList);
+  };
 
   return (
     <div className="App">
       <div className="todoListWrapper">
         <Header />
-        <TodoList todoList={todoList} />
+        <Counter />
+        <AddTodo addTodo={addTodo} />
+        <TodoList
+          todoList={todoList}
+          checkDone={checkDone}
+          removeTodo={removeTodo}
+        />
       </div>
     </div>
   );
--- react/week2/my-app/src/components/todoList.js
+++ react/week2/my-app/src/components/todoList.js
@@ -1,12 +1,43 @@
 import React from "react";
-import Todo from "./todo";
 
-const TodoList = ({ todoList }) => {
+const Todo = ({ todo, checkDone, removeTodo }) => {
+  return (
+    <li
+      id={todo.id}
+      key={todo.id}
+      className={todo.done ? "todoWrapper strike" : "todoWrapper"}
+    >
+      {todo.title}
+      <input
+        type="checkbox"
+        id={todo.id}
+        onChange={(e) => checkDone(e.target.id)}
+        defaultChecked={todo.done}
+      />
+      <button id="delete" onClick={() => removeTodo(todo.id)}>
+        Delete
+      </button>
+    </li>
+  );
+};
+
+const TodoList = ({ todoList, checkDone, removeTodo }) => {
   return (
     <ul>
-      {todoList.map((todo) => {
-        return <Todo key={todo.id} todo={todo} />;
-      })}
+      {todoList.length === 0 ? (
+        <h2>No items</h2>
+      ) : (
+        todoList.map((todo) => {
+          return (
+            <Todo
+              key={todo.id}
+              todo={todo}
+              checkDone={checkDone}
+              removeTodo={removeTodo}
+            />
+          );
+        })
+      )}
     </ul>
   );
 };
